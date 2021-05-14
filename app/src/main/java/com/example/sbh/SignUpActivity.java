aab30277b09afb14cdb8b0a24531e410e97ade96
nicobers <nbers@bbns.org>
2021-05-14 09:32:03 -0400

    new pages

--- app/src/main/java/com/example/sbh/SignUpActivity.java
+++ app/src/main/java/com/example/sbh/SignUpActivity.java
@@ -1,5 +1,8 @@
 package com.example.sbh;
 
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,14 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        if(checkBox.isChecked()){
+            EditText editTextPhone = findViewById(R.id.editTextPhone);
+            EditText editTextPostalAddress = findViewById(R.id.editTextTextPostalAddress);
+            EditText editTextBusinessName = findViewById(R.id.editTextBusinessName);
+            editTextPhone.setVisibility(View.VISIBLE);
+            editTextBusinessName.setVisibility(View.VISIBLE);
+            editTextPostalAddress.setVisibility(View.VISIBLE);
+        }
     }
 }
