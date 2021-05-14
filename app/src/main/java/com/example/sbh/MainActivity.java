2dc3558a941d5ff6ad6732058e6adf2080eb3cb1
pablosegovia <psegovia04@gmail.com>
2021-05-14 13:27:48 -0400

    commit

--- app/src/main/java/com/example/sbh/MainActivity.java
+++ app/src/main/java/com/example/sbh/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sbh;
 
+import android.view.View;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signUp = (Button)findViewById(R.id.signUp);
     }
 }
