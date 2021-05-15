324ce56085b2233293dc09d505bc9e5156e8c70f
nicobers <nbers@bbns.org>
2021-05-14 21:15:38 -0400

    working version

--- app/src/main/java/com/example/sbh/MainActivity.java
+++ app/src/main/java/com/example/sbh/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sbh;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.widget.*;
@@ -13,8 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button signUpBtn = (Button)findViewById(R.id.signUp);
+        Button logInBtn = findViewById(R.id.login2);
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(startIntent);
+            }
+        });
+        Button signUpBtn = findViewById(R.id.signup);
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
