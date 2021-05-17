be260c4ffa6d9299e13e4a5def0ba36b62abb5b0
elizabeth_chin <echin@bbns.org>
2021-05-16 20:56:36 -0400

    commit

---
+++ app/src/main/java/com/example/sbh/BusinessAccountActivity.java
@@ -0,0 +1,13 @@
+package com.example.sbh;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BusinessAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
--- app/src/main/java/com/example/sbh/ConsumerAccountActivity.java
+++ app/src/main/java/com/example/sbh/ConsumerAccountActivity.java
@@ -3,11 +3,11 @@ package com.example.sbh;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AccountActivity extends AppCompatActivity {
+public class ConsumerAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        setContentView(R.layout.activity_consumer_account);
     }
 }
