e9b527f0112e8f55f1cbe9882d238f636f17d8f8
elizabeth_chin <echin@bbns.org>
2021-05-16 21:23:24 -0400

    commit

--- app/src/main/java/com/example/sbh/BusinessAccountActivity.java
+++ app/src/main/java/com/example/sbh/BusinessAccountActivity.java
@@ -8,6 +8,6 @@ public class BusinessAccountActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        setContentView(R.layout.activity_business_account);
     }
 }
