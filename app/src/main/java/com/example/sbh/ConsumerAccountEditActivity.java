75c7445b89e52805db972dc290733de3105ad99e
elizabeth_chin <echin@bbns.org>
2021-05-20 10:09:09 -0400

    commit

---
+++ app/src/main/java/com/example/sbh/ConsumerAccountEditActivity.java
@@ -0,0 +1,13 @@
+package com.example.sbh;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsumerAccountEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consumer_account_edit);
+    }
+}
