6fa5c11c36f3e5315eed97a76c039988a4643323
daniellercb <daniellercbrennan@gmail.com>
2021-05-21 12:54:11 -0400

    Login stuff

--- app/src/main/java/com/example/sbh/Account.java
+++ app/src/main/java/com/example/sbh/Account.java
@@ -14,9 +14,25 @@ public class Account {
 		this.password=password;
 		this.address=address;
 		this.idNum = idNum;
-		accounts = new ArrayList<Account>();
+		accounts = new ArrayList<>();
 		accounts.add(this);
+		insertionSort();
 	}
+
+	public void insertionSort(){
+		// Java program for implementation of Insertion Sort
+		for (int j = 1; j < accounts.size(); j++) {
+			Account current = accounts.get(j);
+			int i = j-1;
+			while ((i > -1) && ((accounts.get(i).getEmail().compareTo(current.getEmail())) == 1)) {
+				accounts.set(i+1, accounts.get(i));
+				i--;
+			}
+			accounts.set(i+1, current);
+		}
+	}
+
+
 
 	public boolean getUserType () {
 		return isBusiness;
--- app/src/main/java/com/example/sbh/LoginActivity.java
+++ app/src/main/java/com/example/sbh/LoginActivity.java
@@ -1,13 +1,67 @@
 package com.example.sbh;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-
+    //haven't figured out what the current account is yet, will know after login so this info can be used to access correct data for other pages that succeed the login.
+    Account currentAcc = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.signUpButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                //create reference variable for the email at this instance state
+                String email = ((EditText) findViewById(R.id.editTextTextEmailAddress)).getText().toString();
+                //do the same for the entered password
+                String password = ((EditText) findViewById(R.id.editTextTextPassword)).getText().toString();
+
+                if(loginButton.isPressed()&&loginPermitted(email, password)){
+                    Intent startIntent = new Intent(getApplicationContext(), HomeScreen.class);
+                    startActivity(startIntent);
+                }
+            }
+
+            public boolean loginPermitted(String email, String password){
+                currentAcc = Account.accounts.get(binarySearchAccountsString(email, 0, Account.accounts.size()-1));
+
+                //if email is found and password is correct, permit login, otherwise do not.
+                return binarySearchAccountsString(email, 0, Account.accounts.size() - 1) > -1 && (currentAcc.getPassword().equals(password));
+            }
+
+
+            public int binarySearchAccountsString(String email, int l, int r){
+
+                if (r >= l)
+                {
+                    int mid = l + (r - l) / 2;
+
+                    if (Account.accounts.get(mid).getEmail().compareTo(email) == 0)
+                        return mid;
+
+                    if (Account.accounts.get(mid).getEmail().compareTo(email) > 0)
+                        return binarySearchAccountsString(email, l, mid - 1);
+
+                    return binarySearchAccountsString(email, mid + 1, r);
+                }
+
+                // We reach here when element is not present
+                // in array
+                return -1;
+            }
+
+
+        });
+
+
     }
 }
