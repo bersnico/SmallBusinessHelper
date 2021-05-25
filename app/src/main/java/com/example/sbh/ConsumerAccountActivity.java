b28652dd132f45fa10efa13536f5362cb115319d
elizabeth_chin <echin@bbns.org>
2021-05-25 11:47:45 -0400

    commit

--- app/src/main/java/com/example/sbh/ConsumerAccountActivity.java
+++ app/src/main/java/com/example/sbh/ConsumerAccountActivity.java
@@ -3,6 +3,7 @@ package com.example.sbh;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,6 +13,14 @@ public class ConsumerAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consumer_account);
+
+        TextView cEmail = findViewById(R.id.textView7);
+        String currEmail = LoginActivity.currentCAcc.getEmail();
+        cEmail.setText(currEmail);
+
+        TextView cLocal = findViewById(R.id.textView11);
+        String currLocal = LoginActivity.currentCAcc.getLocation();
+        cLocal.setText(currLocal);
 
         Button logInBtn = findViewById(R.id.logout2);
         logInBtn.setOnClickListener(new View.OnClickListener() {
