59ad8a1801a729000c0ca8e7a0b68b01274579b7
elizabeth_chin <echin@bbns.org>
2021-05-26 00:21:39 -0400

    commit

--- app/src/main/java/com/example/sbh/BusinessAccountActivity.java
+++ app/src/main/java/com/example/sbh/BusinessAccountActivity.java
@@ -3,6 +3,7 @@ package com.example.sbh;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,6 +13,31 @@ public class BusinessAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_account);
+
+        TextView cBName = findViewById(R.id.textView6);
+        String currBName = LoginActivity.currentBAcc.getName();
+        cBName.setText(currBName);
+
+        TextView cEmail = findViewById(R.id.textView7);
+        String currEmail = LoginActivity.currentBAcc.getEmail();
+        cEmail.setText(currEmail);
+
+        //no favorite variable yet
+//        TextView cFavorites = findViewById(R.id.textView6);
+//        String currFav = LoginActivity.currentBAcc.get();
+//        cFavorites.setText(currFav);
+
+        TextView cPrice = findViewById(R.id.textView11);
+        String currPrice = LoginActivity.currentBAcc.getPriceRange() + "";
+        cBName.setText(currBName);
+
+        TextView cCat = findViewById(R.id.textView12);
+        String currCat = LoginActivity.currentBAcc.getCategory();
+        cCat.setText(currCat);
+
+        TextView cLoc = findViewById(R.id.textView15);
+        String currLoc = LoginActivity.currentBAcc.getLocation();
+        cLoc.setText(currLoc);
 
         Button edit = findViewById(R.id.edit);
         edit.setOnClickListener(new View.OnClickListener() {
